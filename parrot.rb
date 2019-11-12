22a46dd7af8ae74d0dfd0d9e6559a011664859e8
Luke Lin <lukelin1991@yahoo.com>
2019-11-12 19:41:25 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(name = "Squawk")
+def parrot(name = "Pretty bird!")
   puts "Squawk!"
+  return "Squawk"
+  
 end
